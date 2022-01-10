f75c17025fa0c8b1604c7d87c6912ee512da9e14
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-10 20:38:57 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,14 @@
-provider " aws" {
-  profile = "default"
+provider "aws" {
+ profile = "default"
   region = "us-east-2"
-  }
-resource "aws_instance" "myec2" {
-  ami ="ami-0fb653ca2d3203ac1"
+}
+resource "aws_instance" "ec2" {
+   ami ="ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
   subnet_id = "subnet-04bb8948"
-  user_data = "${file("userdada.sh")}"
+  security_groups = ["sg-0862e0d8235267a61"]
+ user_data = "${file("userdata.sh")}"
   tags = {
-    name="ec2"
+    Name = "ec2_s3"
   }
+}
