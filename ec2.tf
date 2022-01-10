5c7d68ef7a00128ab6206d1f8c7ea03e821bd32c
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-10 20:42:04 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,6 @@ resource "aws_instance" "ec2" {
    ami ="ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
   subnet_id = "subnet-04bb8948"
-  security_groups = ["sg-0862e0d8235267a61"]
  user_data = "${file("userdata.sh")}"
   tags = {
     Name = "ec2_s3"
